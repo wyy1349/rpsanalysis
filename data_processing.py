eca4ac1bb008b1349e2d29a6fefc609ecc410daa
Elaine Wang <elaineyw@mit.edu>
2022-12-01 11:32:23 -0500

    helper functions for up/down/stay

--- data_processing.py
+++ data_processing.py
@@ -5,9 +5,22 @@ import numpy as np
 with open('rps_v1_data.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
     for row in spamreader:
-        #print(', '.join(row))
+        print(', '.join(row))
     
 #use pandas to extract the correct columns from csv file
+
+#helper function: convert a sequence of two plays to "UP", "DOWN", "STAY"
+#key1: the first play, key 2: the second play. value1: whether the first play is higher, lower, or the same as the second play; value2: is first -> second going up, down or staying
+res = {"paper":{"scissors":("L","UP"),"rock":("W","DOWN"),"paper":("T","STAY")},
+    "scissors":{"paper":("W","DOWN"),"rock":("L","UP"),"scissors":("T","STAY")},
+    "rock":{"paper":("L","UP"),"scissors":("W","DOWN"),"rock":("T","STAY")}}
+
+def compare(key1,key2):
+    return res[key1][key2][1]
+
+#helper function: convert a sequence of plays, length n, to a sequence of "UP", "DOWN", "STAY", length n-1
+def compare_seq(seq):
+    return [compare(seq[i],seq[i+1]) for i in range(len(seq)-1)]
 
 #translate both into rps and win/lose/tie
 
